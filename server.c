f482d682348cf521882163672c595c65c65cafe2
Dmitry Kuratnik <devmobby@gmail.com>
2014-05-30 00:42:04 +0200

    Final version, formatting
    Final version, formatting

--- server.c
+++ server.c
@@ -8,9 +8,6 @@
 #define SERVER_PORT 12345
 #define QUEUE_SIZE 5
 
-
-
-
 void send_file(char* filename, int socket)
 {
 	int bytes_read;
@@ -39,7 +36,7 @@ void *thread_func(void *sock)
 {
 	int asock = *((int*)sock);
 	char buffer[80];
-	int error = recv(asock, buffer, sizeof(buffer), 0);
+	int error = recv(asock, buffer, sizeof(buffer), 0); // last parameter - flags
 
 	// send file
 	send_file(buffer, asock);
@@ -48,5 +45,59 @@ void *thread_func(void *sock)
 
 void main (int argc, char *argv[])
 {
+	int error;
+	int listen_sd;
 
+	int accept_sd;
+	char buffer[80];
+	struct sockaddr_in addr;
+	pid_t child_pid;
+
+	// create listening socket
+	listen_sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sd < 0) {
+		perror("socket() failed");
+		exit(-1);
+	}
+
+	// setoption error
+	if (error < 0) {
+		perror("setsockopt() failed");
+		close(listen_sd);
+		exit(-1);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // converts a u_long from host to TCP/IP network byte order (which is big-endian).
+	addr.sin_port  = htons(SERVER_PORT);
+	error = bind(listen_sd, (struct sockaddr *)&addr, sizeof(addr));
+
+	if (error < 0) {
+		perror("bind() failed");
+		close(listen_sd);
+		exit(-1);
+   	}
+
+	error = listen(listen_sd, QUEUE_SIZE);
+	if (error < 0) {
+		perror("listen() failed");
+		close(listen_sd);
+		exit(-1);
+	}
+
+	printf("The server is ready\n");
+
+	while (1) {
+		accept_sd = accept(listen_sd, NULL, NULL);
+		if (accept_sd < 0) {
+			perror("accept() failed");
+			close(listen_sd);
+			exit(-1);
+		}
+		// here start new thread
+		pthread_t pthread;
+		pthread_create(&pthread, NULL, &thread_func, (void*)&accept_sd);
+	}
+	close(listen_sd);
 }
